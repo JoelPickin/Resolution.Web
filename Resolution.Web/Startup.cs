00f66618e8839df22dd9c109502d076db3259559
Joel Pickin <joelpickin@outlook.com>
2021-01-17 16:47:11 +0000

    * Added Dashboard to nav list. * Registered singleton of InMemoryData

--- Resolution.Web/Startup.cs
+++ Resolution.Web/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Resolution.Web.Data;
+using Resolution.Web.Data.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,7 @@ namespace Resolution.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IResolutionData, InMemoryResolutionData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
